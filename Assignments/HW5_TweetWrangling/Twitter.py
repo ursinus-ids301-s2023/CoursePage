1209eb8acd9cd8dee88167a5ddb8a657d1a41f02
Chris Tralie <chris.tralie@gmail.com>
2023-03-19 23:27:04 -0400

    Updating HW5

--- Assignments/HW5_TweetWrangling/Twitter.py
+++ Assignments/HW5_TweetWrangling/Twitter.py
@@ -1,20 +1,35 @@
-"""
-Programmer: Chris Tralie / IDS 301 Class
-Purpose: To load in Trump's tweets since 11/1/2016, and
-         to do some data wrangling on them
-"""
-
-import pickle
-import json
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
+import numpy as np
+import json
 
-# A dictionary for converting from the 3 letter months
-# that Twitter gives back to a 2-digit month string
+## Months final, for the plot_coronavirus_timeline function
 MONTHS = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
 
+def get_tweet_date(tweet):
+    year = ""
+    month = ""
+    day = ""
+    if "created_at" in tweet:
+        date = tweet['created_at']
+        # Separate out date into components in a list
+        # Each element is a different component separated
+        # by a space
+        fields = date.split() 
+        
+        # The year is the last field
+        year = fields[-1]
+        
+        # Use the dictionary defined at the top of the file
+        # to convert from a three letter month to a two digit month
+        month = MONTHS[fields[1]] 
+
+        # This magic code formats a day to be 2 digits, potentially
+        # with a leading zero
+        day = "%02d"%int(fields[2]) 
+    elif "date" in tweet:
+        date = tweet["date"].split()[0]
+        year, month, day = date.split("-")
+    return year + "/" + month + "/" + day
 
 def get_devices(tweets):
     """
@@ -25,15 +40,8 @@ def get_devices(tweets):
     tweets: list of dictionary
         A list of tweets, each of which is a dictionary
     """
-    devices = {} # The dictionary will hold "device name":counts
-    for tweet in tweets: # This is one way to loop through a list
-        device = tweet['source']
-        # If the device hasn't been seen yet, we need to make
-        # A key for it in the dictionary
-        if not (device in devices):
-            devices[device] = 0 # Set the initial count to be zero
-        devices[device] += 1 # Add one to the counts for this device
-    print(devices)
+    ## TODO: Fill this in
+    pass
 
 def get_most_favorited(tweets):
     """
@@ -43,38 +51,9 @@ def get_most_favorited(tweets):
     tweets: list of dictionary
         A list of tweets, each of which is a dictionary
     """
-    # First, we setup a parallel numpy array with the same
-    # number of elements as there are tweets
-    counts = np.zeros(len(tweets))
-    # Then, this loop fills the list with the retweet counts
-    for i, tweet in enumerate(tweets):
-        counts[i] = tweet['retweet_count']
-    # Finally, we can use the nifty "argmax" function in
-    # numpy to pull out the index with the maximum counts
-    max_index = np.argmax(counts)
-    # We then use this index back in the original tweets list
-    # to print out the tweet with the maximum counts
-    print(tweets[max_index])
-
-def get_tweet_date(tweet):
-    date = tweet['created_at']
-    # Separate out date into components in a list
-    # Each element is a different component separated
-    # by a space
-    fields = date.split() 
-    # The year is the last field
-    year = fields[-1]
-    # Use the dictionary defined at the top of the file
-    # to convert from a three letter month to a two digit month
-    month = MONTHS[fields[1]] 
-    # This magic code formats a day to be 2 digits, potentially
-    # with a leading zero
-    day = "%02d"%int(fields[2]) 
-    return year + "/" + month + "/" + day
+    ## TODO: Fill this in
+    pass
 
 
-#tweets = pickle.load(open("trumpSinceElection.dat", "rb"))
-tweets = json.load(open("trumpSinceElection.json", "r"))
-get_devices(tweets)
-get_most_favorited(tweets)
-print(get_tweet_date(tweets[0]))
+tweets = json.load(open("tweets_01-08-2021.json"))
+
